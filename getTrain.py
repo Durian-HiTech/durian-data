388d9f26fa7d7144ac3fa5202a579cdaeed2f263
ztx <1213587739@qq.com>
2021-07-12 19:15:36 +0800

    feat: 火车爬虫

--- getTrain.py
+++ getTrain.py
@@ -3,7 +3,7 @@ import datetime
 import json
 import os
 import time
-
+import pymysql
 import requests
 from requests.exceptions import Timeout
 from tqdm import tqdm
@@ -13,9 +13,9 @@ from utils.logger import create_logger
 
 URL = 'http://train.qunar.com/qunar/checiInfo.jsp'
 DEFAULT_DATE = datetime.datetime.now()
-DEFAULT_DATE_STR = DEFAULT_DATE.strftime('%Y-%m-%d')  # 默认查询当天的
-
-mingming = [('G', 9999),
+#DEFAULT_DATE_STR = DEFAULT_DATE.strftime('%Y-%m-%d')  # 默认查询当天的
+DEFAULT_DATE_STR = "2021-07-13"
+mingming = [('G', 2899),
             ('C', 9999),
             ('Z', 9999),
             ('T', 9999),
@@ -24,7 +24,7 @@ mingming = [('G', 9999),
             ('Y', 999)]
 
 
-def getinfo(logger, train_number='G99', date=DEFAULT_DATE_STR):
+def getinfo(logger,cursorx,db,train_number='G2850', date=DEFAULT_DATE_STR):
     # logger.info('getinfo')
     params = {
         'method_name': 'buy',
@@ -44,13 +44,38 @@ def getinfo(logger, train_number='G99', date=DEFAULT_DATE_STR):
         logger.error(params)
         return None
     result = json.loads(response.text)
-    if result == {'count': 0}:
-        logger.error('无法获取该火车班次，班次为' + train_number)
-        return None
-    return result
+    print(result)
+    # for i in result['trainInfo']:
+    #     # print(i)
+    #     print("班次:",result['trainInfo'][i]['code'])
+    #     print("出发日期:",result['trainInfo'][i]['dptDate'])
+    #     print("到达日期:",result['trainInfo'][i]['arrDate'])
+    #     print("始发地:",result['trainInfo'][i]['deptCity'])
+    #     print("始发站:",result['trainInfo'][i]['deptStation'])
+    #     print("目的地:",result['trainInfo'][i]['arriCity'])
+    #     print("到达站:",result['trainInfo'][i]['arriStation'])
+    #     print("出发时间:",result['trainInfo'][i]['deptTime'])
+    #     print("到达时间:",result['trainInfo'][i]['arriTime'])
+    try:
+        for i in result['trainInfo']:
+            cursorx.execute(
+            """insert into train_domestic(departure_city, departure_time,arrival_city,arrival_time,train_number,departure_station,arrival_station)
+            value (%s,%s,%s,%s,%s,%s,%s)""",  # 纯属python操作mysql知识，不熟悉请恶补
+            (result['trainInfo'][i]['deptCity'],
+            result['trainInfo'][i]['dptDate']+' '+result['trainInfo'][i]['deptTime'],
+            result['trainInfo'][i]['arriCity'],
+            result['trainInfo'][i]['arrDate']+' '+result['trainInfo'][i]['arriTime'],
+            result['trainInfo'][i]['code'],
+            result['trainInfo'][i]['deptStation'],
+            result['trainInfo'][i]['arriStation'],
+            ))
+
+            db.commit()
+    except:
+        pass        
 
 
-def main(verbose, path, index=0, st=1):
+def main(verbose, path, index=0, st=0):
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -67,7 +92,7 @@ def main(verbose, path, index=0, st=1):
             else:
                 time.sleep(1)
             name = mingming[current_index][0] + str(current_st)
-            result = getinfo(logger=logger, train_number=name)  # result是json
+            result = getinfo(logger=logger, train_number=name,db=db,cursorx=cursor)  # result是json
             # print(result)
             if result:
                 data = list(result['trainInfo'].values())[0]
@@ -77,7 +102,7 @@ def main(verbose, path, index=0, st=1):
                 with open(os.path.join(path, '火车班次爬到哪了.txt'), 'w', encoding='utf-8') as fp:
                     fp.write(f'index={current_index}, st={current_st+1}\n')
                 with open(os.path.join(path, '火车班次json数据.json'), 'a', encoding='utf-8') as fp:
-                    fp.write(json.dumps(result) + '\n')
+                    fp.write(json.dumps(result,ensure_ascii=False) + '\n')
                 with open(os.path.join(path, '火车班次列表.json'), 'a', encoding='utf-8') as fp:
                     fp.write(name + '\n')
             else:
@@ -91,7 +116,7 @@ def local_main():
     parser.add_argument('--verbose', required=False, default=True, type=bool)
     parser.add_argument('--path', required=False, default=os.path.join('spider_train', 'train_spider'), type=str)
     parser.add_argument('--index', required=False, default=0, type=int)
-    parser.add_argument('--st', required=False, default=1, type=int)
+    parser.add_argument('--st', required=False, default=2850, type=int)
     args = parser.parse_args()
     
     start = datetime.datetime.now().strftime('[%m-%d %H:%M:%S]')
@@ -107,6 +132,8 @@ def local_main():
         print(f'爬完了！')
 
 if __name__ == '__main__':
+    db = pymysql.connect(host = "rm-uf6ji600qianqe6921o.mysql.rds.aliyuncs.com",port=3306,user = "buaase2021",passwd = "buaase(2021)",db = "durian")
+    cursor = db.cursor()
     local_main()
 
     # res = getinfo('G1317')
