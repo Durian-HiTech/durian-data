16714250178cd072926d63f04d3e14768376c8d2
Coach257 <45021066+Coach257@users.noreply.github.com>
2021-07-13 15:14:56 +0800

    国家中转英字典格式

--- utils/countryen2zh.py
+++ utils/countryen2zh.py
@@ -4,14 +4,12 @@ def cmp(a):
     return a["value"]
 
 data = json.load(open('World.json',"r",encoding="utf-8"))
-out = []
+out = {}
 for item in data["features"]:
     enname = item["properties"]["NAME"]
     zhname = item["properties"]["CHINESE_NAME"]
-    out.append({
-        "value":enname,
-        "label":zhname
-    })
-out.sort(key = lambda x :cmp(x))
+    out[enname] = zhname
 
-json.dump(out,open("countryen2zh.json","w",encoding="utf-8"),ensure_ascii=False)
+
+
+json.dump(out,open("countryen2zh2.json","w",encoding="utf-8"),ensure_ascii=False)
